03cc9bc16615532c3066acc770f66e72e1e7f269
casxai <joeynicole99@gmail.com>
2023-09-29 16:16:35 +0800

    (2) joey

--- GLfrontend/src/router/index.js
+++ GLfrontend/src/router/index.js
@@ -15,12 +15,12 @@ const router = createRouter({
     //   component: HomeView
     // },
     {
-      path: '/',
+      path: '/signup',
       name: 'signup',
       component: SignupView
     },
     {
-      path: '/login',
+      path: '/',
       name: 'login',
       component: LoginView
     },
