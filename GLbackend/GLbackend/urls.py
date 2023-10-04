95c4ba68ed147145c1249820d24d8e3a7b451ab9
casxai <joeynicole99@gmail.com>
2023-10-04 15:30:15 +0800

    view/urls commit

--- GLbackend/GLbackend/urls.py
+++ GLbackend/GLbackend/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('api/', include('account.urls')), 
+    path('api/posts/', include('post.urls')),
     path('admin/', admin.site.urls),
 
     # if you're intending to use the browsable API you'll probably also want to add REST framework's login and logout views. uncomment the code below to enable this feature
