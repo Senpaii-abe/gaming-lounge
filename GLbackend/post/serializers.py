5477791e2894ada0d2d895de19c9a64c558a33ca
Senpaii-abe <52749536+Senpaii-abe@users.noreply.github.com>
2023-10-11 22:48:52 +0800

    Added trends... Part 9 31mins

--- GLbackend/post/serializers.py
+++ GLbackend/post/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 
 from account.serializers import UserSerializer
 
-from .models import Post, Comment
+from .models import Post, Comment, Trend
  
 class PostSerializer(serializers.ModelSerializer):
     created_by = UserSerializer(read_only=True) #read only because when we create post we dont want to mess with the created_by field
@@ -26,3 +26,8 @@ class PostDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ('id', 'body', 'likes_count', 'comments_count', 'created_by', 'created_at_formatted','comments',)
+
+class TrendSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Trend 
+        fields = ('id','hashtag', 'occurences',)
