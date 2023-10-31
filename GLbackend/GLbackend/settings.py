1833d7398d959e7cfdf70973ceef7cbf5f822af4
casxai <joeynicole99@gmail.com>
2023-10-31 23:54:25 +0800

    weird but okay

--- GLbackend/GLbackend/settings.py
+++ GLbackend/GLbackend/settings.py
@@ -108,13 +108,13 @@ WSGI_APPLICATION = 'GLbackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
         
-    }
-}
+#     }
+# }
 
 # DATABASES = {
 #   'default': {
@@ -131,7 +131,15 @@ DATABASES = {
 #   }
 # }
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
